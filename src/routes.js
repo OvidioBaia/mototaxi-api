c190d7418a145ec1e88e058a4299b82b2d669e27
Ruan Oliveira <ruan.oliv85@gmail.com>
2022-08-23 13:11:48 -0300

    att routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const asyncHandler = require("express-async-handler");
 const pontoController = require("./app/controllers/ponto/PontoController")
 const usuarioController =  require("./app/controllers/usuario/UsuarioController")
+const enderecoController =  require("./app/controllers/endereco/EnderecoController")
+const usuario_has_pontoController =  require("./app/controllers/usuario_has_ponto/Usuario_has_pontoController")
 
 const routes = new Router();
 
@@ -28,5 +30,26 @@ routes.get("/usuario/:id", asyncHandler(usuarioController.findOne));
 routes.delete("/usuario/:id", asyncHandler(usuarioController.delete));
 
 
+// endereco End-points
+routes.post("/endereco", asyncHandler(enderecoController.store));
+
+routes.get("/endereco", asyncHandler(enderecoController.index));
+
+routes.put("/endereco/:id", asyncHandler(enderecoController.update));
+
+routes.get("/endereco/:id", asyncHandler(enderecoController.findOne));
+
+routes.delete("/endereco/:id", asyncHandler(enderecoController.delete));
+
+// usuario_has_ponto End-points
+routes.post("/usuario_has_ponto", asyncHandler(usuario_has_pontoController.store));
+
+routes.get("/usuario_has_ponto", asyncHandler(usuario_has_pontoController.index));
+
+routes.put("/usuario_has_ponto/:id", asyncHandler(usuario_has_pontoController.update));
+
+routes.get("/usuario_has_ponto/:id", asyncHandler(usuario_has_pontoController.findOne));
+
+routes.delete("/usuario_has_ponto/:id", asyncHandler(usuario_has_pontoController.delete));
 
 module.exports = routes;
